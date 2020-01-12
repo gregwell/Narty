98f81da268112387c312bb95912fb0bfc10073e2
DESKTOP-27CQTIN\grzegurz <grzegorzstudzinski@hotmail.com>
2020-01-12 12:05:25 +0100

    zmiana komentarzy w mainie

--- Main.cpp
+++ Main.cpp
@@ -1,22 +1,16 @@
-#include "source.cpp"
-
-#include "pch.h"
+﻿#include "pch.h"
 #include <iostream>
 #include "header.h"
 
 int main()
 {
-	std::cout << "Hello World!\n";
-	funkcja();
+
+	std::cout << "Witaj w programie Narty33 - najlepszej aplikacji do zarzadzania nartami na swiecie" << endl;
+
 	wybierz_usluge();
 
 	Pracownik p;
 
-}
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-	system("pause");
 	return 0;
 }
+
